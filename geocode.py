b45bdea495d7caacfd4f47e780b9556cb2295b81
John <jwmp5051@gmail.com>
2025-07-22 15:13:53 +0100

    Get coordinates by location name, zip or postcode. Reverse lookup from coordinates.

--- geocode.py
+++ geocode.py
@@ -11,7 +11,7 @@ load_dotenv("D:/Python/EnvironmentVariables/.env")
 APP_ID = os.environ.get("APIKey-OpenWeatherMap-Python")
 
 
-def geo_location(city, country_code=None):
+def geo_location(city: str, country_code: str = None) -> tuple[float, float, str]:
     """
     Get geographical coordinates by location name
     Country Codes: https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
@@ -30,7 +30,7 @@ def geo_location(city, country_code=None):
     params = {
         'q': query,
         'appid': APP_ID,
-        'limit': 1,
+        'limit': 10,
     }
     response = requests.get(url=url, params=params)
     # print(response.text)
@@ -56,8 +56,95 @@ def geo_location(city, country_code=None):
     return first_result["lat"], first_result["lon"], first_result["country"]
 
 
+def geo_zip(zip_code: str, country_code: str = None) -> tuple[float, float, str]:
+    """
+    Get geographical coordinates by ZIP or Postcade
+    Country Codes: https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+
+    :param zip_code: ZIP or Postcade to search for
+    :type zip_code: str
+    :param country_code: Country to search in (optional)
+    :type country_code: str
+    :return: lattitude, longitude, country code
+    :rtype: tuple[float, float, str]
+    """
+    url = "http://api.openweathermap.org/geo/1.0/zip"
+    query = f'{zip_code}'
+    if country_code:
+        query += f',{country_code}'
+    params = {
+        'zip': query,
+        'appid': APP_ID,
+    }
+    response = requests.get(url=url, params=params)
+    # print(response.text)
+    """
+    {
+      "zip":"NN10",
+      "name":"Wymington",
+      "lat":52.2688,
+      "lon":-0.6027,
+      "country":"GB"
+    }
+    """
+    response.raise_for_status()
+    first_result = response.json()
+    return first_result["lat"], first_result["lon"], first_result["country"]
+
+
+def geo_reverse(lat: float, lon: float) -> str:
+    """
+    Get location name from geographical coordinates
+
+    :param lat: Latitude to search for
+    :type lat: float
+    :param lon: Longitude to search for
+    :type lon: float
+    :return: Location name
+    :rtype: str
+    """
+    url = "http://api.openweathermap.org/geo/1.0/reverse"
+    params = {
+        'lat': lat,
+        'lon': lon,
+        'appid': APP_ID,
+        'limit': 1,
+    }
+    response = requests.get(url=url, params=params)
+    print(response.text)
+    """
+    [
+      {
+        "name":"Shire Of Donnybrook-Balingup",
+        "local_names":{
+          "en":"Shire Of Donnybrook-Balingup"
+        },
+        "lat":-33.7022605,
+        "lon":115.99736970337452,
+        "country":"AU",
+        "state":"Western Australia"
+      }
+    ]
+    """
+    response.raise_for_status()
+    city = response.json()[0]["name"]
+    return city
+
+
 if __name__ == '__main__':
-    lat, lon, code = geo_location("Perth")
-    print(lat, lon, code)
-    lat, lon, code = geo_location("Perth", "gb")
-    print(lat, lon, code)
+    # FIND LOCATION BY CITY NAME
+    # lat, lon, code = geo_location("Perth")
+    # print(lat, lon, code)
+    # lat, lon, code = geo_location("Perth", "gb")
+    # print(lat, lon, code)
+
+    # FIND LOCATION BY ZIP / POSTCODE
+    # lat, lon, code = geo_zip("6239", "au")
+    # print(lat, lon, code)
+    # lat, lon, code = geo_zip("NN10", "gb")
+    # print(lat, lon, code)
+
+    city_name = geo_reverse(51.5128, -0.0918)
+    print(city_name)
+    city_name = geo_reverse(-33.5622, 115.8524)
+    print(city_name)
